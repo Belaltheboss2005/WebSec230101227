16c2ca61be281468820192982e9d9dd2e548204c
Belaltheboss2005 <152273746+Belaltheboss2005@users.noreply.github.com>
2025-02-22 20:14:57 +0200

    recreated the project because it wasn't working. IT WORKS....YAY

--- WebSecService/resources/views/welcome.blade.php
+++ WebSecService/resources/views/welcome.blade.php
@@ -4,17 +4,21 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap Test</title>
-    <link href="public/css/bootstrap.min.css" rel="stylesheet">
-    <script src="public/js/bootstrap.bundle.min.js"></script>
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
 </head>
 <body>
-    @php($j = 5)
-    <div class="card m-4 col-sm-2">
+@php($j = 5)
+    <div class="card m-4">
     <div class="card-header">{{$j}} Multiplication Table</div>
     <div class="card-body">
     <table>
-    @foreach (range(1, 10) as $i)
-    <tr><td>{{$i}} * {{$j}}</td><td> = {{ $i * $j }}</td></li>
+    @foreach (range(1, 100) as $i)
+        @if (isPrime($i))
+            <span class ="badge bg-primary m-1">{{$i}}&nbsp;</span>
+        @else
+        <span class ="badge bg-secondary m-1">{{$i}}&nbsp;</span>
+        @endif
     @endforeach
     </table>
     </div>
