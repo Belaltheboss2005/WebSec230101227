ba132d9c98c79163f9fd1ba461a10f903a5486c9
Belaltheboss2005 <152273746+Belaltheboss2005@users.noreply.github.com>
2025-02-22 21:21:59 +0200

    Assignment 1 Done, based on lecture 2 video and lecture 1 PDF(test.blade.php)

--- WebSecService/resources/views/welcome.blade.php
+++ WebSecService/resources/views/welcome.blade.php
@@ -1,27 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap Test</title>
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
-</head>
-<body>
-@php($j = 5)
+@extends('layouts.master')
+@section('title', 'Welcome')
+@section('content')
     <div class="card m-4">
-    <div class="card-header">{{$j}} Multiplication Table</div>
-    <div class="card-body">
-    <table>
-    @foreach (range(1, 100) as $i)
-        @if (isPrime($i))
-            <span class ="badge bg-primary m-1">{{$i}}&nbsp;</span>
-        @else
-        <span class ="badge bg-secondary m-1">{{$i}}&nbsp;</span>
-        @endif
-    @endforeach
-    </table>
+        <div class="card-body">
+            Welcome to Home Page
+        </div>
     </div>
-    </div>
-</body>
-</html>
+@endsection
--- WebSecService/routes/web.php
+++ WebSecService/routes/web.php
@@ -1,16 +1,22 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome'); //welcome.blade.php
 });
-Route::get('/multable', function () {
-    return view('multable'); //multable.blade.php
+Route::get('/multable', function (Request $request) {
+    $j = $request->number ??2;
+    $msg = $request->msg ??'';
+    return view('multable', compact('j',"msg")); //multable.blade.php
 });
 Route::get('/even', function () {
     return view('even'); //even.blade.php
 });
 Route::get('/prime', function () {
     return view('prime'); //prime.blade.php
+});
+Route::get('/test', function () {
+    return view('test'); //test.blade.php
 });
